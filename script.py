bbe3afd57db73911364c7849e010c046c3a75409
Kyra Young <kyrayoung124@gmail.com>
2023-10-13 17:48:36 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,14 @@
 class Menu:
-  def __init__(self, name, items, start_time, end_time)
+  def __init__(self, name, items, start_time, end_time):
     self.name = name
     self.items = items
     self.start_time = start_time
     self.end_time = end_time
-
-  brunch_name = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11:00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3:50}
+  
+  brunch_name = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
   early_bird_menu = {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 15.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,}
   dinner_menu = {'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00,}
-  kids_menu = {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}
+  kids_menu = {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}            
 
   brunch_name = Menu('brunch', brunch_items, 1100, 1600)  
   early_bird_menu = Menu('early_bird', early_bird_menu, 1500, 1800)
@@ -16,6 +16,15 @@ class Menu:
   kids_menu = Menu('kids', kids_menu, 1100, 2100)
 
 def __repr__(self):
-  return self.representative_string
+  return self.name + 'menu available from ' + str(self.start_time) + ' - ' + str(self.end_time)
+#print(brunch)
 
-  print(brunch)
+def calculate_bill(self, purchased_items):
+  current_bill = 0
+  for purcased_item in purchased_items:
+    if purchased_item in self.items:
+      current_bill += self.items[purcased_item]
+  return current_bill
+
+print(brunch_menu.calculate_bill(['pancakes', 'home fries', 'coffee']))
+print(early_bird.calculate_bill(['salumeria plate', 'vegan mushroom ravioli']))
